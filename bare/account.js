5f98e98a5a1a888cc262ddc06c1dcb9b3dff8b25
n9lsjr <nilsjr@protonmail.com>
2024-10-07 19:27:04 +0200

    Add rpc and downloadDir to bare account class

--- bare/account.js
+++ bare/account.js
@@ -1,3 +1,4 @@
+const ce = require('compact-encoding');
 class Account {
   constructor() {
     //Random test data
@@ -7,18 +8,28 @@ class Account {
     this.avatar = '';
     this.rooms = [];
     this.downloadDir = '';
+    this.sender = null;
   }
 
-  init(data) {
+  init(data, sender) {
     this.name = data.name;
     this.address = data.address;
     this.avatar = data.avatar;
+    this.sender = sender;
+    this.downloadDir = data.downloadDir;
   }
 
   update(data) {
     this.name = data.name;
     this.address = data.address;
     this.avatar = data.avatar;
+  }
+
+  send(type, data) {
+    console.log('Send rpc data from bare');
+    const send = data;
+    send.type = type;
+    this.sender.write(JSON.stringify(send));
   }
 }
 
