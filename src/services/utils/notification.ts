1a27b36332950a59f2d6b7925be420bf5d773dad
n9lsjr <nilsjr@protonmail.com>
2024-12-04 21:00:47 +0100

    Add vibration and message notify sound

--- src/services/utils/notification.ts
+++ src/services/utils/notification.ts
@@ -11,18 +11,21 @@ export async function notify({ text, name }, type: string) {
   const channelId = await notifee.createChannel({
     id: 'hugin_notifiy',
     name: 'Hugin',
+    sound: 'roommessage',
+    vibration: true,
+    vibrationPattern: [200, 400],
   });
 
   // Display a notification
   await notifee.displayNotification({
     android: {
       channelId: channelId,
+
       // optional, defaults to 'ic_launcher'.
       // pressAction is needed if you want the notification to open the app when pressed
       pressAction: {
         id: 'default',
       },
-
       smallIcon: '@mipmap/ic_launcher',
     },
     body: text,
