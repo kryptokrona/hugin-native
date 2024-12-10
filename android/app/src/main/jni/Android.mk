07360053991b09559e5d7504f9af73086d916fb8
n9lsjr <nilsjr@protonmail.com>
2024-12-10 23:52:19 +0100

    Update android crypto make file

--- android/app/src/main/jni/Android.mk
+++ android/app/src/main/jni/Android.mk
@@ -1,7 +1,22 @@
 LOCAL_PATH := $(call my-dir)
 
+# ===============================
+# TurtleCoin_jni Module
+# ===============================
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := TurtleCoin_jni
-LOCAL_SRC_FILES := TurtleCoin.cpp crypto.cpp crypto-ops.cpp crypto-ops-data.cpp hash.cpp keccak.cpp
+LOCAL_SRC_FILES := \
+    Java.cpp \
+    crypto.cpp \
+    hash.cpp \
+    keccak.cpp \
+    StringTools.cpp \
+    kryptokrona.cpp \
+    multisig.cpp \
+    crypto-ops.cpp \
+    crypto-ops-data.cpp
 LOCAL_LDLIBS := -llog
+LOCAL_CPPFLAGS += -fexceptions
+
 include $(BUILD_SHARED_LIBRARY)
