35feb49563e07804869a844608992e4f6a1744b3
n9lsjr <nilsjr@protonmail.com>
2024-12-16 08:49:51 +0100

    Update chat colors

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -5,9 +5,22 @@ export const getColorFromHash = (hash: string): string => {
   }
 
   let color = '#';
+  const rgb = [];
+
   for (let i = 0; i < 3; i++) {
     const value = (hashValue >> (i * 8)) & 0xff;
+    rgb.push(value);
     color += ('00' + value.toString(16)).slice(-2);
+  }
+  const brightness = (0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]) / 255;
+  if (brightness < 0.7) {
+    const brightenedRgb = rgb.map((component) =>
+      Math.min(255, Math.floor(component + (255 - component) * 0.7)),
+    );
+    color = '#';
+    brightenedRgb.forEach((component) => {
+      color += ('00' + component.toString(16)).slice(-2);
+    });
   }
 
   return color;
