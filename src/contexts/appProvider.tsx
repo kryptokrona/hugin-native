1eb4c57bfc2c3eafe31e50c2c6c9783329cf227b
Tilda Höglund <48841764+pofffff@users.noreply.github.com>
2025-01-31 11:23:41 +0100

    remove multiple listener injections

--- src/contexts/appProvider.tsx
+++ src/contexts/appProvider.tsx
@@ -2,32 +2,22 @@ import { useEffect } from 'react';
 
 import { AppState, Platform, SafeAreaView } from 'react-native';
 
-import { bare } from 'lib/native';
+import { bare, keep_alive, send_idle_status } from 'lib/native';
 import { Connection, Files } from 'services/bare/globals';
 
 import {
   getCurrentRoom,
   setStoreCurrentRoom,
-  updateUser,
   useGlobalStore,
   usePreferencesStore,
   useThemeStore,
   useUserStore,
-  useRoomStore,
-  getThisRoom,
 } from '@/services';
 import { sleep } from '@/utils';
 
-import { Foreground } from './service';
-
-import {
-  joinRooms,
-  leaveRooms,
-  setLatestMessages,
-  setLatestRoomMessages,
-} from '../services/bare';
+import { joinRooms, setLatestRoomMessages } from '../services/bare';
 import { keychain } from '../services/bare/crypto';
-import { getContacts, initDB, loadSavedFiles } from '../services/bare/sqlite';
+import { initDB, loadSavedFiles } from '../services/bare/sqlite';
 import { MessageSync } from '../services/hugin/syncer';
 import { Wallet } from '../services/kryptokrona/wallet';
 import { Timer } from '../services/utils';
@@ -36,62 +26,93 @@ interface AppProviderProps {
   children: React.ReactNode;
 }
 
-let started = false;
-let joining = false;
-
 export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
   const theme = useThemeStore((state) => state.theme);
   const authenticated = useGlobalStore((state) => state.authenticated);
   const user = useUserStore((state) => state.user);
   const preferences = usePreferencesStore((state) => state.preferences);
-  const { setThisRoom } = useRoomStore();
+
+  let ReactNativeForegroundService: any;
+
+  useEffect(() => {
+    const loadForegroundService = async () => {
+      if (Platform.OS === 'android') {
+        const { default: Service } = await import(
+          '@supersami/rn-foreground-service'
+        );
+        ReactNativeForegroundService = Service;
+      }
+    };
+    loadForegroundService();
+  }, []);
+
+  const addTask = async () => {
+    if (Platform.OS === 'android') {
+      ReactNativeForegroundService?.add_task(() => keep_alive(), {
+        delay: 10000,
+        onError: (e) => console.error('Error starting task', e),
+        onLoop: true,
+        taskId: 'hugin',
+      });
+    }
+  };
+
+  const startTask = async () => {
+    try {
+      await ReactNativeForegroundService.start({
+        ServiceType: 'dataSync',
+        button: false,
+        button2: false,
+        button2Text: 'Stop',
+        buttonOnPress: 'cray',
+        buttonText: 'Close',
+        color: '#000000',
+        icon: 'ic_launcher',
+        id: 1244,
+        message: 'Syncing messages...',
+        setOnlyAlertOnce: 'true',
+        title: 'Hugin',
+        visibility: 'public',
+      });
+    } catch (error) {
+      console.error('Failed to start foreground service', error);
+    }
+  };
 
   async function init() {
-    //Check if android background task is already running
-
     if (Platform.OS === 'android') {
-      const err = await Foreground.init();
-      if (err) {
-        return;
-      }
+      await addTask();
+      await startTask();
     }
 
     await initDB();
     await setLatestRoomMessages();
-    await setLatestMessages();
 
-    let node = {};
-    if (preferences?.node === undefined) {
-      node = { port: 80, url: 'node.xkr.network' };
-    } else {
-      node = {
-        port: parseInt(preferences.node.split(':')[1]),
-        url: preferences.node.split(':')[0],
-      };
-    }
+    const node = preferences?.node
+      ? {
+          port: parseInt(preferences.node.split(':')[1]),
+          url: preferences.node.split(':')[0],
+        }
+      : { port: 80, url: 'node.xkr.network' };
+
     Connection.listen();
     await Wallet.init(node);
 
-    const contacts = await getContacts();
-    const knownKeys = [];
-    for (const contact of contacts) {
-      knownKeys.push(contact.messagekey);
-    }
     const keys = Wallet.privateKeys();
-    MessageSync.init(node, knownKeys, keys);
+    const contacts = [
+      'ec5bc96b9e2431fbe146d23de585acc9cad32ac1adaf412f830dc68985fa6d27',
+      '7368d6437260c59e5cc2609d8baa2b038bea03c14fd77db8e026678aaa63624b',
+    ]; // KNOWN pub keys from db
+    MessageSync.init(node, contacts, keys);
 
-    //Set this somewhere in a state?
+    console.log('huginAddress');
     const huginAddress = Wallet.address + keychain.getMsgKey();
-
     console.log('huginAddress', huginAddress);
-
-    await updateUser({ huginAddress });
 
     Files.update(await loadSavedFiles());
     await bare(user);
     await sleep(100);
     await joinRooms();
-    started = true;
   }
 
   useEffect(() => {
@@ -101,54 +122,39 @@ export const AppProvider: React.FC<AppProviderProps> = ({ children }) => {
     }
   }, [authenticated, user?.address]);
 
-  if (Platform.OS === 'android') {
-    AppState.addEventListener('blur', () => {
-      // do something when the app closed
-      console.log('App not in focus.');
+  useEffect(() => {
+    const Timeout = new Timer(() => {
+      console.log('Stop task after inactivity');
+      if (Platform.OS === 'android') {
+        ReactNativeForegroundService?.stopAll();
+      }
     });
-  }
 
-  const stopTasks = () => {
-    Foreground.service.stopAll();
-  };
-
-  const Timeout = new Timer(() => {
-    //Stop bg tasks after 1hour of inactivity.
-    console.log('Stop task!');
-
-    if (Platform.OS === 'android') {
-      stopTasks();
-    }
-  });
-
-  AppState.addEventListener('change', onAppStateChange);
-  async function onAppStateChange(state: string) {
-    if (state === 'inactive') {
-      console.log('Inactive state');
-      if (Platform.OS === 'ios') {
-        const thisRoom = getCurrentRoom();
-        setThisRoom(thisRoom);
-        await leaveRooms();
-      }
-      //I think this is for iPhone only
-    } else if (state === 'background') {
-      console.log('Start timer');
-      Timeout.start();
-      //Start background timer to shut off foreground task?
-    } else if (state === 'active') {
-      if (started && !joining) {
-        joining = true;
-        if (Platform.OS === 'ios') {
-          const currentRoom = getThisRoom();
-          setStoreCurrentRoom(currentRoom);
-          await joinRooms();
+    const onAppStateChange = async (state: string) => {
+      if (state === 'inactive') {
+        console.log('Inactive state');
+        setStoreCurrentRoom(getCurrentRoom());
+      } else if (state === 'background') {
+        console.log('Start timer');
+        Timeout.start();
+        send_idle_status(true);
+        setStoreCurrentRoom(getCurrentRoom());
+      } else if (state === 'active') {
+        console.log('App active');
+        Timeout.reset();
+        if (Platform.OS === 'android') {
+          send_idle_status(false);
+          setStoreCurrentRoom(getCurrentRoom());
         }
-        joining = false;
       }
-      console.log('Reset timer');
-      Timeout.reset();
-    }
-  }
+    };
+
+    const subscription = AppState.addEventListener('change', onAppStateChange);
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
 
   return (
     <SafeAreaView style={{ backgroundColor: theme.background, flex: 1 }}>
